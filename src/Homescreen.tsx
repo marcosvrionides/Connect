84b06508dd7336c98427f89cddfaabc6e5606c03
vrionides2000 <vrionides2000@gmail.com>
2023-07-23 16:25:44 +0100

    posts' images are displayed

--- src/Homescreen.tsx
+++ src/Homescreen.tsx
@@ -2,20 +2,31 @@ import { ScrollView, StyleSheet, StatusBar, View, Text } from 'react-native';
 import Colours from './Colours';
 import ActionsButton from './ActionsButton';
 import PostCard from './PostCard';
+import { firebase } from '@react-native-firebase/database';
+import { useEffect, useState } from 'react';
 
 function Homescreen(): JSX.Element {
+
+    const [postIds, setPostIds] = useState<string[]>([]);
+
+    useEffect(() => {
+        const reference = firebase
+            .app()
+            .database('https://studentsthoughtsfyp-default-rtdb.europe-west1.firebasedatabase.app/')
+            .ref('/posts')
+            .on('value', snapshot => {
+                const data = snapshot.val();
+                const ids = Object.keys(data);
+                setPostIds(ids);
+            });
+    }, []);
+
     return (
         <View style={styles.container}>
             <ScrollView style={styles.posts}>
-                <PostCard postID={1} hasMedia={true} />
-                <PostCard postID={2} hasMedia={false} />
-                <PostCard postID={3} hasMedia={false} />
-                <PostCard postID={1} hasMedia={true} />
-                <PostCard postID={2} hasMedia={false} />
-                <PostCard postID={3} hasMedia={false} />
-                <PostCard postID={1} hasMedia={true} />
-                <PostCard postID={2} hasMedia={false} />
-                <PostCard postID={3} hasMedia={true} />
+                {postIds.map(id => (
+                    <PostCard key={id} postID={id} />
+                ))}
             </ScrollView>
             <ActionsButton />
         </View>
--- src/PostCard.tsx
+++ src/PostCard.tsx
@@ -1,39 +1,77 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, TextInput } from 'react-native';
 import Colours from './Colours'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { firebase } from '@react-native-firebase/database';
+import storage from '@react-native-firebase/storage';
 
-function PostCard(props): JSX.Element {
+function PostCard(props): Promise<JSX.Element> {
 
-    const postID = props.postID;
-    const hasMedia = props.hasMedia;
+    const [postData, setPostData] = useState(null);
+    const [postImageURL, setPostImageURL] = useState(null);
+    // const date = new Date(postData.timestamp)
+    // const hours = String(date.getHours()).padStart(2, '0');
+    // const minutes = String(date.getMinutes()).padStart(2, '0');
+    // const day = String(date.getDate()).padStart(2, '0');
+    // const month = String(date.getMonth() + 1).padStart(2, '0');
+    // const year = String(date.getFullYear()).slice(2); // Get the last two digits of the year
+    // const formattedDate = `${hours}:${minutes} - ${day}/${month}/${year}`;
+
+    useEffect(() => {
+        // Fetch the post data from Firebase using the postID prop
+        const reference = firebase
+            .app()
+            .database('https://studentsthoughtsfyp-default-rtdb.europe-west1.firebasedatabase.app/')
+            .ref('/posts/' + props.postID);
+
+        reference.on('value', async (snapshot) => {
+            const data = snapshot.val();
+            setPostData(data);
+
+            if (data.file !== 'no file') {
+                // Fetch the image URL from Firebase Storage
+                try {
+                    const url = await storage().ref(snapshot.key).getDownloadURL();
+                    setPostImageURL(url);
+                } catch (error) {
+                    // Handle any errors that occur while fetching the image URL
+                    console.error('Error fetching image URL:', error);
+                }
+            }
+        });
+
+        // Clean up the listener when the component unmounts
+        return () => {
+            reference.off();
+        };
+
+    }, [props.postID]);
+
+    if (!postData) {
+        // Post data is still being fetched, or no data found
+        return null;
+    }
 
     return (
         <View style={styles.container}>
             <View style={styles.topSection}>
-                {/* <Image style={styles.profilePic} /> */}
                 <Ionicons name="person-circle" size={50} color={Colours.primary} />
                 <View style={styles.nameDate}>
-                    <Text style={styles.posterName}>username</Text>
-                    <Text style={styles.postDate}>01:05 - 22/07/23</Text>
+                    <Text style={styles.posterName}>{postData.displayName}</Text>
+                    <Text style={styles.postDate}>{'formattedDate'}</Text>
                 </View>
             </View>
-            <Text style={styles.postText}>
-                This is a post This is a post This is a post This is a post This is a post This is a post
-                This is a post This is a post This is a post This is a post
-            </Text>
-            {hasMedia && <Image style={styles.mediaPreview} />}
-            <Text>{postID}</Text>
+            <Text style={styles.postText}>{postData.content}</Text>
+            {postData.file !== 'no file' && <Image style={styles.mediaPreview} source={{ uri: postImageURL }} resizeMode="contain"/>}
             <View style={styles.bottomSection}>
                 <TextInput style={styles.commentInput} />
                 <MaterialCommunityIcons name='comment-minus-outline' size={25} color={Colours.primary} />
                 <FontAwesome name='heart-o' size={25} color={Colours.primary} />
                 <Feather name='send' size={25} color={Colours.primary} />
             </View>
-
         </View>
     );
 }
@@ -86,7 +124,9 @@ const styles = StyleSheet.create({
     },
     mediaPreview: {
         width: '100%',
-        height: 500,
+        aspectRatio: 1,
+        minHeight: 100,
+        maxHeight: 500,
         borderWidth: 2,
         borderColor: Colours.primary,
         borderRadius: 25,

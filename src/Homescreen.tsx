ea1ab80e45704027b7ea0673447f566fa355d61a
vrionides2000 <vrionides2000@gmail.com>
2023-07-27 12:24:14 +0100

    feed is sorted by time

--- src/Homescreen.tsx
+++ src/Homescreen.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, StatusBar, View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, StyleSheet, StatusBar, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import Colours from './Colours';
 import ActionsButton from './ActionsButton';
 import PostCard from './PostCard';
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 
 function Homescreen(): JSX.Element {
 
-    const [postIds, setPostIds] = useState<string[]>([]);
+    const [postIds_timestamps, setPostIds_timestamps] = useState([])
 
     useEffect(() => {
         const reference = firebase
@@ -15,22 +15,31 @@ function Homescreen(): JSX.Element {
             .database('https://studentsthoughtsfyp-default-rtdb.europe-west1.firebasedatabase.app/')
             .ref('/posts')
             .on('value', snapshot => {
-                const data = snapshot.val();
-                const ids = Object.keys(data);
-                setPostIds(ids);
+                const timestampsData = [];
+                snapshot.forEach(childSnapshot => {
+                    timestampsData.push({
+                        ID: childSnapshot.key,
+                        timestamp: childSnapshot.val().timestamp,
+                    });
+                });
+                // Sort the array by the timestamp property
+                timestampsData.sort((a, b) => b.timestamp - a.timestamp);
+                setPostIds_timestamps(timestampsData);
             });
     }, []);
 
     return (
         <View style={styles.container}>
-            <ScrollView style={styles.posts}>
-                {postIds.map(id => (
-                    <PostCard key={id} postID={id} />
-                ))}
-            </ScrollView>
+            <FlatList
+                style={styles.posts}
+                data={postIds_timestamps}
+                renderItem={({ item }) => <PostCard key={item.ID} postID={item.ID} />}
+                keyExtractor={(item) => item.ID}
+            />
             <ActionsButton />
         </View>
     );
+
 }
 
 const styles = StyleSheet.create({
